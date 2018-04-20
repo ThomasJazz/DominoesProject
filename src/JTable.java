a9447d09921cd1c55be4c86b910208f3dcdc2c94
CwuAl <37880423+CwuAl@users.noreply.github.com>
2018-04-20 13:33:51 -0700

    Matched Formatting with Thomas Branch
    Just match how everything was formatted with the Thomas Branch.

--- src/JTable.java
+++ src/JTable.java
@@ -1,12 +1,10 @@
-package dominosprojectcs380;
-
 import java.util.*;
 
 
 public class JTable {
     public JPlayer[] playerOBJ;
     public JDomino boneyard;
-    private List<DataDomino> dominos; // will contain the two dominos that can be played upon
+    private LinkedList<DataDomino> dominos; // will contain the two dominos that can be played upon
     private int topSide, botSide;
 
     public JTable(JDomino recieveDominoPointerOBJ){
@@ -17,10 +15,10 @@ public class JTable {
 
     public void selecting_pieces(){
         int pieceNo, totalPlayer = 2, pieceWasAvailable;
-        System.out.println("take piece one by one");
+        System.out.println("Taking pieces one by one...");
 
         for (int playerID=0; playerID<totalPlayer; playerID++) {
-            System.out.println("\nPlayer " + playerID + " is Drawing:");
+            System.out.println("\nPlayer " + playerID + " is Drawing...");
             
             for (int i = 0; i < 10; i++) { // not sure why I only had this iterate 12 times before...
                 // get unique pieceNo values from 0-27
@@ -30,7 +28,7 @@ public class JTable {
             }
         }
         
-        System.out.println("\n'Master boneyard' now contains " + boneyard.getMyDominoList().size()
+        System.out.println("\n'The boneyard' now contains " + boneyard.getMyDominoList().size()
         + " pieces: ");
         System.out.println(boneyard.toString());
         System.out.println();
