9d6c0967918f551360f388bd099e1e90a725c91f
CwuAl <37880423+CwuAl@users.noreply.github.com>
2018-04-20 14:10:02 -0700

    Matched the formatting to Master Branch
    Matched the format to the Master Branch.

--- src/Dominos.java
+++ src/Dominos.java
@@ -9,6 +9,7 @@
  *              the boneyard is empty and neither player is able to play
  */
 
+
 public class Dominos {
     private JDomino dominoOBJ;
     private JPlayer[] playerOBJ;
@@ -17,6 +18,7 @@ public class Dominos {
     public static void main(String[] args) {
        new Dominos().play(); // best to leave main method according to my old DS teacher
     }
+    
     public void play(){
         boolean winner = false;
         int pieceID;
@@ -34,23 +36,28 @@ public class Dominos {
         myTableOBJ = new JTable(dominoOBJ);
         myTableOBJ.API(playerOBJ);
 
+        // ************ FOR TESTING ************
         System.out.println("****************************");
         System.out.println("Player 1: " + playerOBJ[0].getHand());
         System.out.println("Player 2: " + playerOBJ[1].getHand());
+
+        System.out.println("Full List:" + dominoOBJ);
+        // ************ FOR TESTING ************
 
         /* for testing
         playerOBJ[0].draw(0);
         System.out.println("New hand after drawing: \n" + playerOBJ[0].getHand());
          */
-        /* now we actually start taking turns.
+        // now we actually start taking turns.
         // real dominoes would have the player with the highest domino go first, but we just
-        start with player[0]*/
+        //start with player[0]
         //myTableOBJ.
         JRandom rand = new JRandom();
         int start = rand.getRandom(0,1); // randomly decide who starts first
 
         for (int i = start; !winner; i++) {
-            int playerTurn = i%2; // use mod 2 to toggle player turn
+            // use mod 2 to toggle player turn
+            int playerTurn = i % 2;
 
             // if the player has a domino they can play, they play it.
             if (canPlay(playerOBJ[playerTurn]) != -1) {
@@ -76,10 +83,12 @@ public class Dominos {
 
         }
     }
-
-    public int canPlay(JPlayer player){
+    
+    // Check which domino the player can play.
+    public int canPlay(JPlayer player) {
         int top = myTableOBJ.getTopSide();
         int bot = myTableOBJ.getBotSide();
+        
         for (int i = 0; i < player.getHand().size(); i++){
             if (player.getHand().get(i).getLeft() == top || player.getHand().get(i).getRight() == top)
                 return i;
@@ -87,6 +96,8 @@ public class Dominos {
             if (player.getHand().get(i).getLeft() == bot || player.getHand().get(i).getRight() == bot)
                 return i;
         }
-        return -1; // if we never find a match in the players hand, we return -1
+        
+        // if we never find a match in the players hand, we return -1
+        return -1;
     }
 }
