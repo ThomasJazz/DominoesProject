ea600752a121d87c222667530ef27864be930c01
CwuAl <37880423+CwuAl@users.noreply.github.com>
2018-04-20 22:49:21 -0700

    Update JDomino.java

--- src/JDomino.java
+++ src/JDomino.java
@@ -1,4 +1,3 @@
-package dominosprojectcs380;
 /* have to use LinkedList instead of Deque because java version of Deque doesn't allow us to access
 * the object at any given index like the C++ version does*/
 import java.util.LinkedList;
